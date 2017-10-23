25b3d9f873cc2b381f48827328210b0ca45fc0c6
Panachai <panachai.ny@gmail.com>
2017-10-23 11:37:33 +0700

    update php my admin

--- www/js/controllers/listControl.js
+++ www/js/controllers/listControl.js
@@ -1,67 +1,57 @@
 angular.module('mycontact')
 
-  .controller('listController', function ($scope, $state) {
+  .controller('listController', function ($scope, $state,$http) {
 
-    //$scope.myDataArray = [{
-    //  name: 'worayot kanle',
-    //  imgDev: 'boy.jpeg',
-    //  nick: 'Boy',
-    //  social: 'fb.com/BoyWorayot',
-    //  mobile: '',
-    //  line: ''
-    //},{
-    //  name: 'Panachai Niyomkodchakorn',
-    //  imgDev: 'golf.png',
-    //  nick: 'Golf',
-    //  social: 'fb.com/panachaiNY',
-    //  mobile: '090-5607225',
-    //  line: 'panachai.ny'
-    //}
-    //    ]
+    //$scope.myDataArray = [
+    //  {
+    //    name: 'ศุภโชค พงษ์จิตปรีอาทร',
+    //    imgDev: 'por.png',
+    //    nick: 'ป้อ',
+    //    social: '-',
+    //    mobile: '-',
+    //    line: '-'
+    //  },
+    //  {
+    //    name: 'ปณชัย นิยมกชกร',
+    //    imgDev: 'golf.png',
+    //    nick: 'กอล์ฟ',
+    //    social: 'fb.com/panachaiNY',
+    //    mobile: '090-560-7225',
+    //    line: 'panachai.ny'
+    //  },
+    //  {
+    //    name: 'อภิษฐา สุนทรธงชัย',
+    //    imgDev: 'cream.png',
+    //    nick: 'ครีม',
+    //    social: 'fb.com/Creammeii',
+    //    mobile: '085-406-6161',
+    //    line: 'creammeii'
+    //  },
+    //  {
+    //    name: 'บุญฤทธิ์ รั้วสำราญ',
+    //    imgDev: 'hok.png',
+    //    nick: 'ฮกคุง',
+    //    social: 'fb.com/hokkung.leo',
+    //    mobile: '083-005-7442',
+    //    line: 'kohok2178'
+    //  },
+    //  {
+    //    name: 'วรยศ คำลือ',
+    //    imgDev: 'boy.png',
+    //    nick: 'บอย',
+    //    social: 'fb.com/BoyWorayot',
+    //    mobile: '084-434-1555',
+    //    line: 'boyworayot'
+    //  }
+    //]
 
 
-    $scope.myDataArray = [
-      {
-        name: 'ศุภโชค พงษ์จิตปรีอาทร',
-        imgDev: 'por.png',
-        nick: 'ป้อ',
-        social: '-',
-        mobile: '-',
-        line: '-'
-      },
-      {
-        name: 'ปณชัย นิยมกชกร',
-        imgDev: 'golf.png',
-        nick: 'กอล์ฟ',
-        social: 'fb.com/panachaiNY',
-        mobile: '090-560-7225',
-        line: 'panachai.ny'
-      },
-      {
-        name: 'อภิษฐา สุนทรธงชัย',
-        imgDev: 'cream.png',
-        nick: 'ครีม',
-        social: 'fb.com/Creammeii',
-        mobile: '085-406-6161',
-        line: 'creammeii'
-      },
-      {
-        name: 'บุญฤทธิ์ รั้วสำราญ',
-        imgDev: 'hok.png',
-        nick: 'ฮกคุง',
-        social: 'fb.com/hokkung.leo',
-        mobile: '083-005-7442',
-        line: 'kohok2178'
-      },
-      {
-        name: 'วรยศ คำลือ',
-        imgDev: 'boy.png',
-        nick: 'บอย',
-        social: 'fb.com/BoyWorayot',
-        mobile: '084-434-1555',
-        line: 'boyworayot'
-      }
-    ]
+    $http.get('http://localhost/www_web_api/webservice/ws_list.php').then(function (response) {
+      console.log(response.data.results);
+      $scope.myDataArray = response.data.results;
+    }, function (error) {
+      console.log(error);
+    });
 
 
     $scope.btnDetail = function (data) {
@@ -69,7 +59,7 @@ angular.module('mycontact')
       $state.go('detail', {
         img: data.imgDev,
         name: data.name,
-        nick: data.nick,
+        nick: data.nickname,
         social: data.social,
         mobile: data.mobile,
         line: data.line
